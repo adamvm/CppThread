e365961467bd5f9909e5df23ef8705dbb4c1368e
kawapa <kawapa@outlook.com>
2019-12-11 13:20:29 +0100

    Another commit

---
+++ Char/others.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <array>
+
+int main()
+{
+    char tab[] = {'h', 'e', 'l', 'l', 'o'};
+//    char tab[] = "hello";
+
+    std::cout << "Table size is: " << sizeof(tab)/sizeof(char) << std::endl;
+
+    for (auto const &el : tab)
+        std::cout << el;
+
+    return 0;
+}
