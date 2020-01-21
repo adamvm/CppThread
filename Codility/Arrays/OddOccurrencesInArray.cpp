241cfa4a7eb72925ef44a23493dd0f7870b8c8fb
Kawalec <kawapa@corp.delaval.com>
2020-01-21 15:11:14 +0100

    Codility Iterations

--- Codility/Arrays/OddOccurrencesInArray.cpp
+++ Codility/Arrays/OddOccurrencesInArray.cpp
@@ -1,13 +1,28 @@
 // https://app.codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/
 
 #include <iostream>
+#include <vector>
+#include <unordered_set>
 
 int solution(std::vector<int> &A);
 
 int main()
 {
-
-
+    std::vector<int> A = {9, 3, 9, 3, 9, 7, 9};
+    std::cout << solution(A);
 
     return 0;
+}
+
+int solution(std::vector<int> &A)
+{
+    std::unordered_set<int> s;
+    for (const auto &i : A)
+    {
+        if (s.find(i) != s.end())
+            s.erase(i);
+        else s.insert(i);
+    }
+    
+    return *s.begin();
 }
